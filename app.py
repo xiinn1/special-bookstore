5c43575a94a8dc79695a41c22c2510acdfd6065c
xiinn1 <117365477+xiinn1@users.noreply.github.com>
2022-11-06 15:27:38 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import requests
+
+def getAllBookstore():
+    url = 'https://cloud.culture.tw/frontsite/trans/emapOpenDataAction.do?method=exportEmapJson&typeId=M'
+    headers = {"accept":"application/json"}
+    respones = requests.get(url,headers=headers)
+    res = respones.json()
+    return res
+
+def getCountyOption(items):
+    optionList = []
+    for item in items:
+        name=item['cityName'][0:3]
+
+        if name in optionList:
+            continue
+        else:
+            optionList.append(name)
+    return optionList
+
+def getDistrictOption(items, target) ->list:
+    optionList = []
+    for item in items:
+        name = item['cityName']
+        if target not in name: continue
+        name.strip()
+        district = name[5:]
+        if len(district) == 0: continue
+        if district not in optionList:
+            optionList.append(district)
+    return optionList
+
+def getSpecificBookstore(items, county, districts):
+    specificBookstoreList = []
+    for item in items:
+        name = item['cityName']
+        if county not in name: continue
+        for district in districts:
+            if district not in name: continue
+            specificBookstoreList.append(item)
+    return specificBookstoreList
+
+
+def getBookstoreInfo(items):
+    expanderList = []
+    for item in items:
+        expander = st.expander(item['name'])
+        expander.image(item['representImage'])
+        expander.metric('hitRate', item['hitRate'])
+        expander.subheader('Introduction')
+        expander.write(item['intro'])
+        expander.subheader('Address')
+        expander.write(item['address'])
+        expander.subheader('Open Time')
+        expander.write(item['openTime'])
+        expander.subheader('Email')
+        expander.write(item['email'])
+        expanderList.append(expander)
+    return expanderList
+
+def app():
+    bookstoreList=getAllBookstore()
+    countyOption=getCountyOption(bookstoreList)
+    st.header('特色書店地圖')
+    st.metric('Total bookstore',len(bookstoreList))
+    county = st.selectbox('請選擇縣市',countyOption)
+    districtOption = getDistrictOption(bookstoreList, county)
+    district = st.multiselect('請選擇區域',districtOption)
+
+    specificBookstore = getSpecificBookstore(bookstoreList, county, district)
+    num = len(specificBookstore)
+    st.write(f'總共有{num}項結果', num)
+
+    specificBookstore.sort(key = lambda item: item['hitRate'], reverse=True)
+    bookstoreInfo = getBookstoreInfo(specificBookstore)
+
+
+if __name__=="__main__":
+    app()
+
